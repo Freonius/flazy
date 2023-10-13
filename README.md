033679219d121a46820729cb35e0aa6b076b78f0
Fred Pirani <freonius@gmail.com>
2023-10-13 12:35:25 +0200

    Initial

---
+++ README.md
@@ -0,0 +1,3 @@
+Some tools that I've used in my development of Flutter applications.
+
+A work in progress.
---
+++ lib/flazy.dart
@@ -0,0 +1,20 @@
+library flazy;
+
+import 'package:flutter/material.dart';
+
+class UB extends StatelessWidget {
+  const UB({
+    super.key,
+  });
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    throw UnimplementedError();
+  }
+}
+
+/// A Calculator.
+class Calculator {
+  /// Returns [value] plus 1.
+  int addOne(int value) => value + 1;
+}
